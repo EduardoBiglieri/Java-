451fe98e62b5c47b5ddad4a33fd5a30f9fc2d874
Eduardo Augusto de Freitas Biglieri <145765979+EduardoBiglieri@users.noreply.github.com>
2024-10-04 12:22:35 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Progamação',
+    'O que é Python?',
+    'O Phython é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Progamação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua Imnglesa',
+    'Como se diz Oi em inglês?',
+    'Oi em inglês é HI (RAI)'
+)
